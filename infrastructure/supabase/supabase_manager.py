b985e8c1ef565d54a056054f7b2386fe48052591
HotelkaWants <shurochka.dev@gmail.com>
2026-03-17 19:05:52 +0300

    supabase backoff impl

--- infrastructure/supabase/supabase_manager.py
+++ infrastructure/supabase/supabase_manager.py
@@ -1,6 +1,7 @@
 from supabase import create_client, Client
 from typing import Optional
 import logging
+import time
 
 logger = logging.getLogger(__name__)
 
@@ -9,13 +10,26 @@ class SupabaseStorageManager:
         self.supabase: Client = create_client(supabase_url, supabase_key)
         self.bucket_name = "analyzed-images" 
     
-    def get_image_by_url(self, image_path: str) -> Optional[bytes]:
-        try:
-            response = self.supabase.storage.from_(self.bucket_name).download(image_path)
-            if response:
-                return response
-            else:
-                return None
-        except Exception as e:
-            logger.error(f"Supabase download error: {e}")
-            return None
+    def get_image_by_url(self, image_path: str, retries: int = 3, backoff: float = 1.5,) -> Optional[bytes]:
+        for attempt in range(retries):
+            try:
+                file_bytes = self.supabase.storage.from_(self.bucket_name).download(image_path)
+
+                if file_bytes:
+                    return file_bytes
+
+                logger.warning("Empty response: %s", image_path)
+
+            except Exception as e:
+                logger.warning(
+                    "Download attempt %d failed: path=%s error=%s",
+                    attempt + 1,
+                    image_path,
+                    e
+                )
+
+            sleep_time = backoff ** attempt
+            time.sleep(sleep_time)
+
+        logger.error("Failed to download after %d attempts: %s", retries, image_path)
+        return None
